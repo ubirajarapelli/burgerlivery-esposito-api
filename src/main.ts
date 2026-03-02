b1053f92cf14bcf53d7a506793dc6c03fc4d33db
Ubirajara Pelli <ubirajaracpelli@gmail.com>
2026-03-02 16:48:44 -0300

    refactor: add cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Pizzalivery API')
